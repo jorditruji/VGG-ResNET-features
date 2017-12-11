bb894015f13db98ecf276ad9fede8d57b3a47d62
jorditruji <jordimoreratrujillo@gmail.com>
2017-12-11 21:43:33 +0100

    pre-trained

--- train_vgg.py
+++ train_vgg.py
@@ -8,8 +8,8 @@ import numpy as np
 from Models import vgg16
 
 
-# MINI VGG AUTOENCODER CUSTOM NET:
-def mini_vgg(input_shape):
+# MINI VGG AUTOENCODER:
+def mini_vgg(input_shape,extra_conv,decoder):
 	#input images shape
 	inputs = Input(input_shape)
 	#CNN Coder
@@ -17,25 +17,32 @@ def mini_vgg(input_shape):
 	out = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(out)
 	out = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(out)
 	out = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(out)
-	out = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(out)
-	out = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(out)
+	if extra_conv:
+		out = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(out)
+		out = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(out)
 	#CNN DECODER:
-	out= Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
-	out = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(out)
-	out = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(out)
-	out = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(out)
-	out = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(out)
-	out = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(out)
+	if decoder:
+		out= Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
+		out = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(out)
+		out = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(out)
+		out = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(out)
+		if extra_conv:
+			out = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(out)
+			out = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(out)
 	#create net
 	depthnet = Model(inputs, out)
 	depthnet.summary()
 
-
+#def pretrained_vgg(input_shape,extra_conv,decoder):
 
 #CREATE VGG CONV NET (FULL CONVNET PRETRAINED WITH IMAGENET)
-#vgg = vgg16.VGG16(include_top=False, weights='imagenet', input_shape=(640, 480, 3))
+vgg = vgg16.VGG16(include_top=False, weights='imagenet', input_shape=(640, 480, 3))
+for i in range(12):
+	vgg.layers.pop()
+vgg.summary()
 inputs = Input(shape=(640, 480, 3))
 #out = Dense(1, name='my_dense',activation='relu')(vgg.layers[-1].output)
+'''
 #CONVOLUTIONAL BLOCK:
 out= Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
 out = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(out)
@@ -44,15 +51,7 @@ out = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'
 #out = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(out)
 #out = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(out)
 
-'''
-#DECONVOLUTIONAL BLOCK FOR FULL VGG16 NET :
-#out =Conv2DTranspose(512,(3, 3),strides=(2, 2), padding='same', name='block5_deconv', activation='relu')(vgg.output)
-#out =Conv2DTranspose(256,(3, 3),strides=(2, 2), padding='same', name='block4_deconv', activation='relu')(out)
-#out =Conv2DTranspose(128,(3, 3),strides=(2, 2), padding='same', name='block3_deconv', activation='relu')(out)
-out =Conv2DTranspose(64,(3, 3),strides=(2, 2), padding='same', name='block2_deconv', activation='relu')(out)
-out =Conv2DTranspose(1,(3, 3),strides=(2, 2), padding='same', name='block1_deconv')(out)
-'''
-#FC+REGRESSION
+#drop+deconv
 out = Dropout(0.5)(out)
 out=Conv2DTranspose(1,(3, 3),strides=(2, 2), padding='same', name='block5_deconv', activation='relu')(out)
 
@@ -60,9 +59,9 @@ out=Conv2DTranspose(1,(3, 3),strides=(2, 2), padding='same', name='block5_deconv
 depthnet = Model(inputs, out)
 depthnet.summary()
 optimizer=RMSprop(lr=0.01)
+'''
 
-
-depthnet.compile(loss='mean_absolute_error', optimizer='adam')
+vgg.compile(loss='mean_absolute_error', optimizer='adam')
 # initialize dataset
 dataset = dataset.dataset(batch_size=16, samples_train=1000, samples_val=300,normalize_depth=False)
 
